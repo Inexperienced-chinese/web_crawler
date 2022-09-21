beb5defd9e35f7a4da166d6db5fb94405b301c94
PavelLovygin <55824376+PavelLovygin@users.noreply.github.com>
2022-09-22 00:53:36 +0500

    add use robot.txt

--- crawler.py
+++ crawler.py
@@ -1,19 +1,27 @@
 from html_parser import HtmlParser
 from setup import CommonSetup
-from downloader import Downloader
+from downloader import Downloader, build_path_to_robot
 from url_utils import UrlUtils
+import urllib.robotparser
+import threading
+
 
 class Crawler:
 
     @staticmethod
     def update():
         for domain in CommonSetup.ALLOWED_DOMAINS:
+            t = threading.Thread(target=Crawler.parse_domain, args=domain)
             Crawler.parse_domain(domain)
 
     @staticmethod
     def parse_domain(domain):
         passed_urls = set()
-        url_stack = [domain]
+        url_stack = [f"https://{domain}"]
+        rp = urllib.robotparser.RobotFileParser()
+        Downloader.download_robot(domain)
+        with open(build_path_to_robot(domain)) as robots_file:
+            rp.parse(robots_file.readlines())
         while len(url_stack) != 0:
             curr_url = url_stack.pop()
             passed_urls.add(curr_url)
@@ -23,7 +31,7 @@ class Crawler:
 
             a = HtmlParser.get_urls_from_html(curr_html_path, curr_url)
             for url in a:
-                # TODO: Реализовать проверку на соответствие robot.txt
-                if url not in passed_urls and \
-                        UrlUtils.get_domain_with_lvl(domain) == UrlUtils.get_domain_with_lvl(url):
+                if rp.can_fetch('*', url) and \
+                        url not in passed_urls and \
+                        UrlUtils.get_domain_with_lvl(f"https://{domain}") == UrlUtils.get_domain_with_lvl(url):
                     url_stack.append(url)
